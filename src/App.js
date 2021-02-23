cc782600f51db76760b7e851aacbd0f40e4f373f
akashahmad <engr.akashahmad@gmail.com>
2021-02-23 12:51:48 +0500

    sign up page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Login from "./container/login";
+import SignUp from "./container/siginUp";
 
 function App() {
   return (
     <>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
     </>
   );
--- src/container/login.jsx
+++ src/container/login.jsx
@@ -1,9 +1,10 @@
 import { Divider, Form, Input, Button } from "antd";
+import { withRouter, Link } from "react-router-dom";
 import Logo from "../components/atoms/logo";
 import Heading from "../components/atoms/heading";
 import Card from "../components/atoms/card";
 import LoginTemplates from "../components/templates/loginTemplates";
-const Login = () => {
+const Login = (props) => {
   const [form] = Form.useForm();
 
   const onFinish = () => {};
@@ -28,18 +29,26 @@ const Login = () => {
             name="email"
             rules={[{ required: true, message: "Please input your email!" }]}
           >
-            <Input className="rounded-3xl" placeholder="Email address" />
+            <Input
+              className="rounded-3xl font-weight-bold"
+              placeholder="Email address"
+            />
           </Form.Item>
           <Form.Item
             label=""
             name="password"
             rules={[{ required: true, message: "Please input your password!" }]}
           >
-            <Input className="rounded-3xl " placeholder="Password" />
+            <Input.Password
+              className="rounded-3xl font-weight-bold"
+              placeholder="Password"
+            />
           </Form.Item>
-          <p className="text-base clr-darkblue cursor-pointer my-3">
-            Forget Password?
-          </p>
+          <Link to="/forget-password">
+            <p className="text-base clr-darkblue cursor-pointer my-3">
+              Forget Password?
+            </p>
+          </Link>
           <div className="text-center my-4">
             <Button
               type="primary"
@@ -49,14 +58,16 @@ const Login = () => {
               Login
             </Button>
           </div>
-          <p className="text-base clr-darkblue cursor-pointer my-3">
-            New to this website? Create Account
-          </p>
+          <Link to="/sign-up">
+            <p className="text-base clr-darkblue cursor-pointer my-3">
+              New to this website? Create Account
+            </p>
+          </Link>
           <div className="text-center my-4">
             <Button
               type="primary"
               className="bg-clr-darkblue rounded-3xl py-1 px-24"
-              htmlType="submit"
+              onClick={() => props?.history?.push("/sign-up")}
             >
               Sign Up
             </Button>
@@ -72,4 +83,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default withRouter(Login);

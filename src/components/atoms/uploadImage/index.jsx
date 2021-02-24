c30c811411f8bd269a653e76ac7d86baa397acfe
akashahmad <engr.akashahmad@gmail.com>
2021-02-24 15:22:50 +0500

    upload image section added

---
+++ src/components/atoms/uploadImage/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Upload } from "antd";
+import "./style.css";
+
+const UploadImg = () => {
+  const [fileList, setFileList] = useState([]);
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+  return (
+    <>
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-card"
+        fileList={fileList}
+        onChange={onChange}
+      >
+        {fileList.length < 1 && "+ Upload"}
+      </Upload>
+    </>
+  );
+};
+
+export default UploadImg;
--- src/components/templates/homePageTemplate.jsx
+++ src/components/templates/homePageTemplate.jsx
@@ -1,8 +1,20 @@
+import { Row, Col } from "antd";
 import Header from "../header";
 import MobileHeader from "../header/mobileHeader";
+import UploadImgSec from "../molecules/uploadImgae";
 
 const HomePageTemplates = (props) => {
-  return !props?.isMobile ? <Header /> : <MobileHeader />;
+  return (
+    <>
+      {!props?.isMobile ? <Header /> : <MobileHeader />}
+      <Row className="mt-6">
+        <Col xs={24} sm={24} md={24} lg={4} xlg={4}></Col>
+        <Col xs={24} sm={24} md={24} lg={20} xlg={20}>
+          <UploadImgSec />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default HomePageTemplates;
